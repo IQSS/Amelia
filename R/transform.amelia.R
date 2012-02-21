148fbd2a56cea8946c4c74561033a4923e5c8b37
mblackwe <mblackwe>
2012-02-21 05:42:00 +0000

    Cleanup for R CMD check

--- R/transform.amelia.R
+++ R/transform.amelia.R
@@ -1,24 +1,24 @@
-transform.amelia <- function(obj, ...) {
+transform.amelia <- function(`_data`, ...) {
   tcall <- match.call(expand.dots = TRUE)
-  if (is.null(obj$transform.calls)) {
-    obj$transform.calls <- list(tcall)
+  if (is.null(`_data`$transform.calls)) {
+    `_data`$transform.calls <- list(tcall)
   } else {
-    obj$transform.calls <- c(obj$transform.calls, tcall)
+    `_data`$transform.calls <- c(`_data`$transform.calls, tcall)
   }
 
 
   tcall[[1]] <- as.name("transform")
   names(tcall)[2] <- ""
-  m <- length(obj$imputation)
+  m <- length(`_data`$imputation)
 
-  orig.data <- remove.imputations(obj)
+  orig.data <- remove.imputations(`_data`)
   tcall[[2]] <- orig.data
   new.miss.matrix <- as.matrix(is.na(eval.parent(tcall)))
 
   for (i in 1:m) {
-    tcall[[2]] <- obj$imputations[[i]]
-    obj$imputations[[i]] <- eval.parent(tcall)
+    tcall[[2]] <- `_data`$imputations[[i]]
+    `_data`$imputations[[i]] <- eval.parent(tcall)
   }
-  obj$missMatrix <- new.miss.matrix
-  return(obj)
+  `_data`$missMatrix <- new.miss.matrix
+  return(`_data`)
 }
