2544c5d8ba9e903e3f697eff357dea40403b5978
Matthew Blackwell <mblackwell@gov.harvard.edu>
2024-11-06 20:44:47 -0500

    fix workflows

--- R/summary.mi.R
+++ R/summary.mi.R
@@ -1,3 +1,15 @@
+#' Summary of an mi object
+#' 
+#' Returns summary information about the list of multiply imputed data
+#' sets
+#'
+#' @param object an object of class \code{mi}. Typically, an output
+#'        from the function \code{amelia}.
+#' @param ... further arguments.
+#'
+#' @seealso \code{\link{amelia}}
+#' @exportS3method
+
 summary.mi <- function(object, ...) {
   m <- length(object)
   nv <- length(object[[1]])
